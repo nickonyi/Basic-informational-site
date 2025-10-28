6aa774491e235f97b1f68f79aa14377d09277bfe
nickonyi <nickoluoch1130@gmail.com>
2025-10-28 14:15:43 +0300

    startic pages using node js

--- index.js
+++ index.js
@@ -7,8 +7,42 @@ const PORT = 3000;
 //create the server
 const server = http.createServer((req, res) => {
   //get the requested url
-  let filepath = "/pages" + req.url;
+  let filepath = "./pages" + req.url;
+
+  //serve the different pages based on the url
+  if (req.url === "/") {
+    filepath = "./pages/index.html";
+  } else if (req.url === "/about") {
+    filepath = "./pages/about.html";
+  } else if (req.url === "/contact") {
+    filepath = "./pages/contact.html";
+  } else {
+    filepath = "./pages/404.html";
+  }
+
+  //set the content type
+  const contentType = "text/html";
+
+  //read and serve the file
   console.log(filepath);
+
+  fs.readFile(filepath, (err, content) => {
+    //if the page does not exist serve a custom 404 page
+    if (err) {
+      if (err.code === "ENOENT") {
+        fs.readFile(filepath, (error, notFound) => {
+          res.writeHead(404, { "Content-Type": contentType });
+          res.end(notFound, "utf-8");
+        });
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
 });
 
 server.listen(PORT, () => {
