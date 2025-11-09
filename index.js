b6488133f29b996c21523de1488127227cd8e401
nickonyi <nickoluoch1130@gmail.com>
2025-11-09 17:04:05 +0300

    basic information site created with express

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ app.use(express.static(path.join(__dirName, "pages")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirName, "pages", "index.html"));
 });
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirName, "pages", "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirName, "pages", "contact.html"));
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirName, "pages", "404.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`listening at port:${PORT}`);
+});
